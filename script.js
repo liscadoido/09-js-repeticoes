868979cfbc3a027103624fb4e214249c63af27a6
liscadoido <132668870+liscadoido@users.noreply.github.com>
2023-06-24 00:40:24 +0000

    Entrega

---
+++ script.js
@@ -0,0 +1,56 @@
+
+function Contar(){
+    let ini = document.getElementById("txti");
+    let fim = document.getElementById("txtf");
+    let passo = document.getElementById("txtp");
+    let res = document.getElementById("res");
+
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0 ){
+        res.innerHTML = "Impossivel contar";
+        window.alert('[ERRO] Prencha todos os campos')
+    }
+    else{
+        res.innerHTML = "Contado: <br>";
+        let inicio = Number(ini.value);
+        let final = Number(fim.value);
+        let passo = Number(passo.value);
+        if(passo <= 0){
+            window.alert('Passo invalido ! Considerando PASSO 1');
+            passo = 1;
+        }
+        if(1 < f){
+            // Contagem crescente
+            for(let c = inicio; c <= final; c += passo){
+                res.innerHTML += `${c} \u{1F449}`;
+            }
+        }
+        else{
+            // Contagem decrescente
+            for(let c = inicio; c <= final; c -= P){
+                res.innerHTML += `${c} \u{1F449}`;
+            }
+        }
+        res.innerHTML += `\u{1F3C1}`;
+
+    }
+
+}
+function tabuada(){
+    let num = document.getElementById("txtn");
+    let tab = document.getElementById("seltab");
+    if(num.value.lenght == 0){
+        window.alert("Por favor digite um numero !");
+    }
+    else{
+        let numero = Number(num.value);
+        let contagem = 1
+        tab.innerHTML = "";
+        while( contagem <=  10){
+            let item = document.createElement("option");
+            item.text = `${numero} x ${contagem} = ${numero*contagem}`;
+            item.value = `tab${contagem}`
+            tab.appendChild(item);
+            c++
+        }
+    }
+}
